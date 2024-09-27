b34b32dbecaaa8c53c96e852ce70f1340429cf49
BunThang <bunthang204@gmail.com>
2024-09-27 13:11:30 +0700

    Check cho do sai

--- Back/index.js
+++ Back/index.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 const ConnectDB = require('./config/db');
-ConnectDB();
+ConnectDB({
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const app = express();
 const port = process.env.PORT || 9999;
-
+app.set('view engine', 'ejs');
 app.use(express.json());
 // Handle 404
+
+app.use(morgan('dev'));
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page!');
+  });
 app.get('*', (req, res) => {
-    res.status(404).render('404');
+    res.status(404).json({ message: 'Page not found' });
 });
 
-app.listen(port, () => console.log(`Listen on the ${port}`))
+app.listen(port,  () => console.log(`Listen on the ${port}`))
